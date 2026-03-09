ce2c2125e2eeca1a5ccc6aad11657171064b12da
crisostomo_e <crisostomo_e@ca01.ub.lan>
2026-03-09 11:47:40 +0800

    Created navigation component and setup the router.

--- midterm-activity-one/src/main.jsx
+++ midterm-activity-one/src/main.jsx
@@ -5,6 +5,8 @@ import App from "/app";
 import "./app.css";
 
 import Home from "/src/pages/Home";
+import Students from "/src/pages/Students";
+import About from "/src/pages/About";
 
 const root = document.getElementById("root");
 
@@ -12,6 +14,8 @@ ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/students" element={<Students />} />
+      <Route path="/about" element={<About />} />
     </Routes>
   </BrowserRouter>,
 );
